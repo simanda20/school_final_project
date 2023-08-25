16207155ad7e003fc0aaa9cd4bb1692d1b11101e
simanda20 <simanda20@sps-prosek.cz>
2023-08-25 14:20:27 +0200

    product processing

--- program/program.py
+++ program/program.py
@@ -1,6 +1,15 @@
+import re # import regex
 from time import sleep # import sleep
 import requests # import request
 from bs4 import BeautifulSoup # import beautiful soup
+
+def process_product(product):
+    product_name = product.find("a", {"class": "name"}).text # get product name
+    product_price_sep = product.find("span", {"class": "price-box__price"}).text # get product price with separation
+    product_price_not_sep = re.sub(r'\s+', '', product_price_sep) # get product price without separation
+    product_price = int(product_price_not_sep.replace(",-", "")) # get rid of ',-' and get int from product price
+    product_code = product.get("data-code") # code of product
+    product_id = product.get("data-id") # product id used by eshop
 
 url = "https://www.alza.cz/televize/18849604-p$page.htm" # prep url
 page = 1 # page
@@ -26,9 +35,7 @@ while has_products: # querry
        first_product = products[0]
 
     for product in products: # process all products
-        # process_product(product)
-        print(product.find("a", {"class": "name"}).text + "      price: " + product.find("span", {"class": "price-box__price"}).text.replace("&nbsp;", " "))
-        continue
+        process_product(product)
 
     page += 1 # load new page
 
