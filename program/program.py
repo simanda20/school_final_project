34e6ab43c66a05f8b1e3fae356b7d0db461c4f23
simanda20 <simanda20@sps-prosek.cz>
2023-09-19 10:49:09 +0200

    update on service

--- program/program.py
+++ program/program.py
@@ -195,7 +195,7 @@ class Querry_Datart(Querry):
 def get_time_difference():
     current_time = datetime.now() # get current time
     next_day = current_time + timedelta(days=1) # get next day
-    next_day = next_day.replace(hour=1, minute=0, second=0, microsecond=0) # get hour of next day
+    next_day = next_day.replace(hour=1, minute=0, second=0, microsecond=0) # get first hour of next day
     time_difference = next_day - current_time # calculate time difference
     return time_difference.total_seconds() # convert time difference to seconds and return
 
